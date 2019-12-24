0f031c4f028c6e3961b01e033af08dfd5218f1a2
Caleb Coffin <coffincw@gmail.com>
2019-12-24 02:45:08 -0500

    added functions for title screen

--- ArduinoVGAConsole.ino
+++ ArduinoVGAConsole.ino
@@ -2,28 +2,46 @@
 #include <ESP32Lib.h>
 #include <Ressources/Font6x8.h>
 #include <Ressources/CodePage437_8x19.h>
+#include <GfxWrapper.h>
 #include <Fonts/FreeMonoBoldOblique24pt7b.h>
 #include <Fonts/FreeSerif9pt7b.h>
 #include <Fonts/TomThumb.h>
 
+#define BUTTON0 34
+#define BUTTON1 0
+#define BUTTON2 35
 
 //pin configuration
-const int redPin = 14;
-const int greenPin = 19;
-const int bluePin = 27;
-const int hsyncPin = 32;
-const int vsyncPin = 33;
+#define REDPIN 14
+#define GREENPIN 19
+#define BLUEPIN 27
+#define HSYNCPIN 32
+#define VSYNCPIN 33
 
 //VGA Device using an interrupt to unpack the pixels from 4bit to 16bit for the I²S
 //This takes some CPU time in the background but is able to fit a frame buffer in the memory
 VGA3Bit vga;
 GfxWrapper<VGA3Bit> gfx(vga, 320, 240);
 
+// shows title screen
 void show_title_screen() {
+  vga.setTextColor(vga.RGB(255, 255, 255));
+  vga.setCursor(140, 120);
+  vga.print("Gameduino");
+}
+
+// shows options screen
+void show_options_screen() {
   
 }
 
+// shows about screen
+void show_about_screen() {
+  
+}
 
+byte prev_state0 = 1, prev_state1 = 1, prev_state2 = 1;
+int button_pressed = 0;
 void setup()
 {
   Serial.begin(115200);
@@ -31,14 +49,27 @@ void setup()
   vga.setFrameBufferCount(2);
   
   //create screen
-  vga.init(vga.MODE640x480.custom(320, 240), redPin, greenPin, bluePin, hsyncPin, vsyncPin);
+  vga.init(vga.MODE640x480.custom(320, 240), REDPIN, GREENPIN, BLUEPIN, HSYNCPIN, VSYNCPIN);
+
+  // initialize buttons
+  pinMode(BUTTON0, INPUT_PULLUP); 
+  pinMode(BUTTON1, INPUT_PULLUP);
+  pinMode(BUTTON2, INPUT_PULLUP);
   
   //setting the font
-  gfx.setFont(&TomThumb);
+  vga.setFont(CodePage437_8x19);
 
   //start title sequence
   show_title_screen();
 
+  while (1) {
+    byte curr_state = digitalRead(BUTTON0);
+    if (prev_state0 == 1 && curr_state == 0) {
+      break;
+    }
+    prev_state0 = curr_state;
+    delay(10);
+  }
   
   
 }
@@ -46,17 +77,18 @@ void setup()
 ///draws a bouncing balls
 void balls()
 {
+  Serial.println("in balls");
   //some basic gravity physics
   static VGA3BitI::Color c[4] = {vga.RGB(0, 255, 0), vga.RGB(0, 255, 255), vga.RGB(255, 0, 255), vga.RGB(255, 255, 0)};
-  static float y[4] = {20, 20, 20, 20};
+  static float y[4] = {50, 50, 50, 50};
   static float x[4] = {20, 20, 20, 20};
-  static float vx[4] = {.01, -0.07, .05, -.03};
+  static float vx[4] = {.1, -0.7, .5, -.3};
   static float vy[4] = {0, 1, 2, 3};
   static unsigned long lastT = 0;
   unsigned long t = millis();
   float dt = (t - lastT) * 0.001f;
   lastT = t;
-  const int r = 6;
+  const int r = 10;
   for (int i = 0; i < 4; i++)
   {
     int rx = r;
@@ -81,8 +113,8 @@ void balls()
       rx = vga.xres - x[i];
     }
     //draw a filled ellipse
-    vga.fillEllipse(x[i], vga.yres - y[i] - 1, rx, ry, c[i]);
-    vga.ellipse(x[i], vga.yres - y[i] - 1, rx, ry, 0);
+    vga.fillCircle(x[i], vga.yres - y[i] - 1, r, c[i]);
+    vga.circle(x[i], vga.yres - y[i] - 1, r, 0);
   }
 }
 
@@ -94,6 +126,7 @@ void loop()
     for (int x = 0; x * 10 < vga.xres; x++)
       vga.fillRect(x * 10, y * 10, 10, 10, (x + y) & 1 ? vga.RGB(255, 0, 0) : vga.RGB(255, 255, 255));
   //text position
+  Serial.println("in loop");
   vga.setCursor(2, 2);
   //black text color no background color
   vga.setTextColor(vga.RGB(0, 0, 0));
